c2c6096ba8a48ceb015035fd43427e00668a785e
Ku-dodo <rnehgus9403@gmail.com>
2023-11-16 11:46:59 +0900

    feat : item.SVC 파일 읽기 기능 추가
    상점 물품을 svc 파일에 추가하여 변경할 수 있도록
    svc 파일을 읽고 리스트에 추가하는 기능 추가

--- TextRPG/Shop.cs
+++ TextRPG/Shop.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextRPG
+﻿namespace TextRPG
 {
     internal class Shop
     {
@@ -13,9 +7,52 @@ namespace TextRPG
 
         public Shop()
         {
-            _items.Add(new Item("목검", "1:3", "나무로 만들어진 검이다.", Item.EType.Weapon, 10));
-            _items.Add(new Item("나무 투구", "2:1", "나무로 만들어진 투구이다.", Item.EType.Armor, 5));
-            _items.Add(new Item("검", "1:5", "흔한 검이다.", Item.EType.Weapon, 3));
+            LoadItemCSV(_items);
         }
+
+        //CSV를 읽어서 리스트를 넣음
+        static void LoadItemCSV(List<Item> _item)
+        {
+            StreamReader itemCSV = new StreamReader(@"..\..\..\ItemData.csv");
+
+            string strCSV = itemCSV.ReadToEnd();
+            string[] CSVLine = strCSV.Split("\n");
+
+            List<Item> StoreItems = new List<Item>();
+
+            for (int i = 0; i < CSVLine.Length; i++)
+            {
+                string[] itemSetting = CSVLine[i].Split(',');
+                string _name = itemSetting[0];
+                Item.EType _type = itemSetting[1] == "Weapon"? Item.EType.Weapon : Item.EType.Armor ;
+
+                int _hp = int.Parse(itemSetting[2]);
+                int _atk = int.Parse(itemSetting[3]);
+                int _def = int.Parse(itemSetting[4]);
+
+                string _status = "";
+                if (_hp != 0)
+                {
+                    _status = $"0:{_hp}";
+                }
+                else if (_atk != 0)
+                {
+                    _status = $"1:{_atk}";
+                }
+                else if (_def != 0)
+                {
+                    _status = $"2:{_def}";
+                }
+
+                string _description = itemSetting[5];
+                int _price = int.Parse(itemSetting[6]);
+
+                Item item = new Item(_name, _status, _description, _type, _price);
+
+                _item.Add(item);
+            }
+
+        }
+
     }
 }
